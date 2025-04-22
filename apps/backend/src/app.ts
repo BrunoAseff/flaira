2ab31f3a33a57c8c9be9548cef49591b619e3c3c
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-04-22 01:38:13 +0000

    feat: add auth routes

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { status } from "@/modules/status/route";
+import { auth } from "./utils/auth";
 
 const app = new Hono();
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
 app.route("/status", status);
 
