2a6f13d11ea0970347252f40cf16f0c294a110bb
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-04-28 16:08:20 +0000

    fix: makes both fileds responsive

--- apps/frontend/src/components/pages/AuthPage.tsx
+++ apps/frontend/src/components/pages/AuthPage.tsx
@@ -32,11 +32,11 @@ export default function AuthPage() {
           </h1>
           <div className="flex flex-col mb-3 gap-1">
             <Label htmlFor="email">Email</Label>
-            <Input name="email" />
+            <Input id="email" name="email" />
           </div>
           <div className="flex flex-col mb-3 gap-1">
-            <Label>Password</Label>
-            <Input />
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" name="password" />
           </div>
 
           <Button>Sign in</Button>
