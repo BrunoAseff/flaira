87dccb6bc9c65e5d0291303158545cf550d9419d
Bruno Aseff <brunoaseff2@gmail.com>
2025-04-30 13:11:08 -0300

    feat: add forgot password page and enhance routes

---
+++ apps/frontend/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div className="w-[85%] md:w-[32rem] bg-background  p-6 rounded-2xl flex flex-col gap-6">
+      <h1 className="text-left mr-auto font-medium text-xl mb-6">
+        Reset your password
+      </h1>
+    </div>
+  );
+}
---
+++ apps/frontend/src/components/pages/SignIn.tsx
@@ -0,0 +1,35 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import Link from "next/link";
+
+export default function SignIn() {
+  return (
+    <div className="w-[85%] md:w-[32rem] bg-background  p-6 rounded-2xl flex flex-col gap-6">
+      <h1 className="text-left mr-auto font-medium text-xl mb-6">
+        Sign in to Flaira
+      </h1>
+      <div className="flex flex-col mb-3 gap-1">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" name="email" type="email" autoComplete="email" />
+      </div>
+      <div className="flex flex-col mb-3 gap-1">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <Link
+          className="text-sm text-blue-700 hover:underline hover:text-blue-600 transition-all duration-300 font-light"
+          href={"/forgot-password"}
+        >
+          Forgot my password
+        </Link>
+      </div>
+
+      <Button>Sign in</Button>
+    </div>
+  );
+}
