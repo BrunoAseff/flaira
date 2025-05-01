17fcc7dc4b0997c3898ce67c1fa7d455d5daddce
Bruno Aseff <brunoaseff2@gmail.com>
2025-05-01 10:54:22 -0300

    style: add icons

--- apps/frontend/src/components/pages/SignIn.tsx
+++ apps/frontend/src/components/pages/SignIn.tsx
@@ -2,20 +2,29 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import Link from "next/link";
+import { Eye, KeyRound, Mail } from "lucide-react";
 
 export default function SignIn() {
   return (
-    <div className="w-[85%] md:w-[32rem] bg-background  p-6 rounded-2xl flex flex-col gap-6">
+    <div className="w-[85%] md:w-[32rem] bg-background p-6 rounded-2xl flex flex-col gap-6">
       <h1 className="text-left mr-auto font-medium text-xl mb-6">
         Sign in to Flaira
       </h1>
       <div className="flex flex-col mb-3 gap-1">
         <Label htmlFor="email">Email</Label>
-        <Input id="email" name="email" type="email" autoComplete="email" />
+        <Input
+          iconLeft={<Mail className="text-accent" />}
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+        />
       </div>
       <div className="flex flex-col mb-3 gap-1">
         <Label htmlFor="password">Password</Label>
         <Input
+          iconLeft={<KeyRound className="text-accent" />}
+          iconRight={<Eye className="text-accent" />}
           id="password"
           name="password"
           type="password"
--- apps/frontend/src/components/ui/input.tsx
+++ apps/frontend/src/components/ui/input.tsx
@@ -23,8 +23,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             "file:text-foreground max-w-[32rem] placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-13 w-full min-w-0 rounded-xl border border-b-3 border-r-3 bg-muted px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
             "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
             "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-            iconLeft && "pl-10",
-            iconRight && "pr-10",
+            iconLeft && "pl-12",
+            iconRight && "pr-12",
             className
           )}
           {...props}
