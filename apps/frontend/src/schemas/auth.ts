be746acfebf07bfffb5e3b0cb60a878620a2d9b9
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-05-14 14:17:41 +0000

    feat: improve schema validation

--- apps/frontend/src/schemas/auth.ts
+++ apps/frontend/src/schemas/auth.ts
@@ -1,21 +1,21 @@
 import { z } from "zod";
 
-const email = z.string().email("Invalid email").trim().toLowerCase();
+const email = z.string().email("Invalid email address").trim().toLowerCase();
 const name = z
   .string()
-  .min(3, "Name is too short")
-  .max(100, "Name is too long")
-  .regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s'-]+$/, "Invalid name");
+  .min(3, "Name must be at least 3 characters")
+  .max(100, "Name cannot exceed 100 characters")
+  .regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s'-]+$/, "Name can only contain letters");
 
 const signUpPassword = z
   .string()
-  .min(8, "Password is too short")
-  .max(100, "Password is too long");
+  .min(8, "Password must be at least 8 characters")
+  .max(100, "Password cannot exceed 100 characters");
 
 const signInPassword = z
   .string()
-  .min(8, "Invalid password")
-  .max(100, "Invalid password");
+  .min(8, "Incorrect password format")
+  .max(100, "Incorrect password format");
 
 export const signUpSchema = z.object({
   name,
