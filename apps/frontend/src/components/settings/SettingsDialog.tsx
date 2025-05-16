326fcaee8e668d31b9a4b60b9aecb28e711a61a8
Bruno Aseff <brunoaseff2@gmail.com>
2025-05-15 22:45:11 -0300

    fix: adjust settingsDialog

--- apps/frontend/src/components/settings/SettingsDialog.tsx
+++ apps/frontend/src/components/settings/SettingsDialog.tsx
@@ -8,32 +8,21 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { LockKeyIcon, UserSquareIcon } from "@hugeicons/core-free-icons";
-import { auth } from "@/auth/client";
-import { useEffect } from "react";
 import SecurityTab from "./SecurityTab";
 
 export function SettingsDialog({
   isOpen,
   setIsOpen,
 }: { isOpen: boolean; setIsOpen: (open: boolean) => void }) {
-  useEffect(() => {
-    async function getSessionsList() {
-      const sessionsList = await auth.listSessions();
-      console.log(sessionsList);
-    }
-
-    getSessionsList();
-  }, []);
-
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="w-[55%] h-[85%] bg-background p-4">
+      <DialogContent className="max-w-4xl w-[90%] md:w-[75%] lg:w-[55%] h-[85%] bg-background p-4">
+        {" "}
         <DialogHeader>
           <DialogTitle className=" ml-5 text-foreground text-2xl font-semibold">
             Settings
           </DialogTitle>
         </DialogHeader>
-
         <div className="my-18 w-full">
           <Tabs
             className="flex w-full"
@@ -66,9 +55,8 @@ export function SettingsDialog({
             <TabsContent className="w-full" value="security">
               <SecurityTab />
             </TabsContent>
-          </Tabs>{" "}
+          </Tabs>
         </div>
-
         <DialogClose />
       </DialogContent>
     </Dialog>
