1d5aae36f2270d5f9a78e5f06b934ce75de7c9cc
Bruno Aseff <brunoaseff2@gmail.com>
2025-05-20 20:10:22 -0300

    fix: adjust dialog overflow #88

--- apps/frontend/src/components/settings/SettingsDialog.tsx
+++ apps/frontend/src/components/settings/SettingsDialog.tsx
@@ -9,27 +9,30 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { LockKeyIcon, UserSquareIcon } from "@hugeicons/core-free-icons";
 import SecurityTab from "./SecurityTab";
+import { Separator } from "../ui/separator";
 
 export function SettingsDialog({
   isOpen,
   setIsOpen,
-}: { isOpen: boolean; setIsOpen: (open: boolean) => void }) {
+}: {
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+}) {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="min-w-full sm:min-w-96 md:min-w-96 w-full md:w-[75%] lg:w-[50%] h-full md:h-[90%] bg-background p-4 overflow-hidden">
-        {" "}
-        <DialogHeader>
-          <DialogTitle className=" ml-5 text-foreground text-2xl font-semibold">
+      <DialogContent className="min-w-full sm:min-w-96 md:min-w-96 w-full md:w-[75%] lg:w-[50%] h-full md:h-[90%] bg-background p-0 flex flex-col overflow-hidden">
+        <DialogHeader className="px-6 py-4 border-b border-muted">
+          <DialogTitle className="text-foreground text-2xl font-semibold">
             Settings
           </DialogTitle>
         </DialogHeader>
-        <div className="my-18 w-full">
+        <div className="flex flex-1 w-full overflow-hidden px-6 pt-4">
           <Tabs
-            className="flex w-full"
+            className="flex w-full h-full"
             defaultValue="profile"
             orientation="vertical"
           >
-            <TabsList className="flex flex-col bg-transparent gap-2 w-fit">
+            <TabsList className="flex flex-col mt-10 min-h-full bg-transparent gap-2 w-fit pr-4">
               <TabsTrigger value="profile">
                 <HugeiconsIcon
                   icon={UserSquareIcon}
@@ -49,10 +52,24 @@ export function SettingsDialog({
                 Security
               </TabsTrigger>
             </TabsList>
-            <TabsContent tabIndex={-1} className="w-full" value="profile">
-              Content 1
+
+            <Separator orientation="vertical" className="h-auto" />
+
+            <TabsContent
+              tabIndex={-1}
+              className="w-full flex-1 h-full overflow-hidden pl-4"
+              value="profile"
+            >
+              <div className="flex flex-col min-h-[80vh] items-center justify-center">
+                Profile Tab Content
+              </div>
             </TabsContent>
-            <TabsContent tabIndex={-1} className="w-full" value="security">
+
+            <TabsContent
+              tabIndex={-1}
+              className="w-full flex-1 h-full overflow-hidden pl-4"
+              value="security"
+            >
               <SecurityTab />
             </TabsContent>
           </Tabs>
