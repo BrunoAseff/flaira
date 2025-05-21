a3af431ef5214ece23f1a24be858e1b7f516b45d
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-05-21 12:25:00 +0000

    fix: add scrollbar stable #90

--- apps/frontend/src/components/settings/SecurityTab.tsx
+++ apps/frontend/src/components/settings/SecurityTab.tsx
@@ -86,7 +86,7 @@ export default function SecurityTab() {
 
   return (
     <div className="w-full flex flex-col h-full">
-      <ScrollArea className="flex-1 pb-16 max-h-[85vh] overflow-y-auto">
+      <ScrollArea className="flex-1 pb-16 max-h-[85vh] scrollbar-gutter-stable overflow-y-auto">
         <div className="w-full py-0 px-4 pb-10">
           <div className="w-full flex flex-col gap-6">
             <div className="flex w-full justify-between">
