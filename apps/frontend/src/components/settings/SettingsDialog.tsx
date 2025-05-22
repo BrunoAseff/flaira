f9653d347cc14288f30aa372daa9c81e4b76c3fe
Bruno Aseff <brunoaseff2@gmail.com>
2025-05-21 22:18:15 -0300

    style: adjust title sizes

--- apps/frontend/src/components/settings/SettingsDialog.tsx
+++ apps/frontend/src/components/settings/SettingsDialog.tsx
@@ -22,7 +22,7 @@ export function SettingsDialog({
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent className="min-w-full sm:min-w-96 md:min-w-96 w-full md:w-[75%] lg:w-[50%] h-full md:h-[90%] bg-background p-0 flex flex-col overflow-hidden">
         <DialogHeader className="px-6 py-4 border-b border-muted">
-          <DialogTitle className="text-foreground text-2xl font-semibold">
+          <DialogTitle className="text-foreground text-xl font-semibold">
             Settings
           </DialogTitle>
         </DialogHeader>
