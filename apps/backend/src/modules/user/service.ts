8a56ff6043ca7f88b09c2c8fc2e88ca377afbe89
Bruno Aseff <brunoaseff2@gmail.com>
2025-05-22 18:33:52 -0300

    fix: add random number to file name

--- apps/backend/src/modules/user/service.ts
+++ apps/backend/src/modules/user/service.ts
@@ -1,14 +1,18 @@
 import { deleteObject, getUrl, uploadUrl } from "@/utils/s3";
+import { v4 as uuidv4 } from "uuid";
 
 export const uploadUserAvatar = async ({
   fileName,
   type,
 }: { fileName: string; type: string }) => {
-  await uploadUrl({ key: `avatar/${fileName}`, type });
+  const id = uuidv4();
+  const url = await uploadUrl({ key: `avatar/${id}/${fileName}`, type });
+  return url;
 };
 
 export const getUserAvatar = async ({ key }: { key: string }) => {
-  await getUrl({ key });
+  const url = await getUrl({ key });
+  return url;
 };
 
 export const deleteUserAvatar = async ({ key }: { key: string }) => {
