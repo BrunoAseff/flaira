75f5f11ea78d95d6afdd0c15ac133fee16176816
Bruno Aseff <brunoaseff2@gmail.com>
2025-06-04 00:12:57 -0300

    fix: simplify user agent usage

--- apps/frontend/src/lib/formatUserAgent.tsx
+++ apps/frontend/src/lib/formatUserAgent.tsx
@@ -1,8 +1,7 @@
 import { UAParser } from "ua-parser-js";
 
 export default function UserAgentDisplay({ userAgent }: { userAgent: string }) {
-  const parser = new UAParser(userAgent);
-  const { browser, device, os } = parser.getResult();
+  const { browser, device, os } = UAParser(userAgent);
 
   const browserName = browser?.name || "Unknown Browser";
 
