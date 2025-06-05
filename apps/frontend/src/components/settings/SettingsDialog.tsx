566faf5c172225bf58ac2ec8bb0ff0ef1092e33b
Bruno Aseff <brunoaseff2@gmail.com>
2025-06-05 14:39:25 -0300

    fix: adjust dialog size

--- apps/frontend/src/components/settings/SettingsDialog.tsx
+++ apps/frontend/src/components/settings/SettingsDialog.tsx
@@ -56,7 +56,7 @@ export function SettingsDialog({
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="min-w-full sm:min-w-96 md:min-w-96 w-full md:w-[75%] lg:w-[50%] h-full md:h-[90%] bg-background p-0 flex flex-col overflow-hidden">
+      <DialogContent className="min-w-full sm:min-w-96 md:min-w-96 w-full md:w-[75%] lg:w-[55%] h-full md:h-[90%] bg-background p-0 flex flex-col overflow-hidden">
         <DialogHeader className="px-6 py-4 border-b border-muted">
           <DialogTitle className="text-foreground text-xl font-semibold">
             Settings
