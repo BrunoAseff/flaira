266950ea5f3d8c15e13304d43aa4dab33ab1b421
Bruno Aseff <brunoaseff2@gmail.com>
2025-06-19 18:46:14 -0300

    feat: update prologue step

--- apps/frontend/src/components/trips/form/steps/Prologue.tsx
+++ apps/frontend/src/components/trips/form/steps/Prologue.tsx
@@ -1,12 +1,32 @@
+import FileInput from "@/components/file-input";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Prologue() {
   return (
     <>
-      <div className="flex mx-12 flex-col gap-4">
-        <Label className="text-xl">What is the title of your trip?</Label>
-        <Input className="w-full rounded-xl" type="text" />
+      <div className="flex flex-col w-full mx-6 md:mx-10 gap-10">
+        <div className="flex flex-col md:flex-row w-full justify-between gap-4">
+          <div className="flex flex-col w-full gap-1">
+            <Label className="text-base">
+              What’s the name of this journey?
+            </Label>
+            <Input className=" rounded-xl" type="text" />
+          </div>
+          <div className="flex flex-col w-full gap-1">
+            <Label className="text-base">
+              How would you describe this experience?
+            </Label>
+            <Textarea className="rounded-xl max-w-[32rem]" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label className="text-base">
+            Add photos or videos that bring it to life
+          </Label>
+          <FileInput />
+        </div>
       </div>
     </>
   );
