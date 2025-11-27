922f1329e537758607a92399ea81df5600bf35c4
Bruno Aseff <brunoaseff2@gmail.com>
2025-11-27 13:23:13 -0300

    refactor: adjust overview typing

--- apps/frontend/src/components/pages/Overview.tsx
+++ apps/frontend/src/components/pages/Overview.tsx
@@ -1,20 +1,28 @@
 import { auth } from '@/auth/client';
 import { api } from '@/lib/api';
 import { useQuery } from '@tanstack/react-query';
-import type { Memory } from '@/types/routes';
+import type { GetRandomMemoriesResponse } from '@/types/routes';
 
 export default function Overview() {
   const { data: session } = auth.useSession();
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ['random-memory', session?.user.image],
+    queryKey: ['random-memory', session?.user.id],
+    enabled: !!session,
     queryFn: async () => {
-      const response = await api.get<Memory[]>('/memory/get-random-memories', {
-        auth: true,
-      });
+      const response = await api.get<GetRandomMemoriesResponse>(
+        '/memory/get-random-memories',
+        {
+          auth: true,
+        }
+      );
 
-      if (response.ok && response.data && response.data.length) {
-        return response.data[0].url;
+      if (
+        response.ok &&
+        response.data?.memories &&
+        response.data.memories.length
+      ) {
+        return response.data.memories[0].url;
       }
     },
   });
