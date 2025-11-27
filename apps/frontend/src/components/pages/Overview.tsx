6cb61f192e8459d71712a6438926fc8db78fc4ee
Bruno Aseff <brunoaseff2@gmail.com>
2025-11-27 13:11:54 -0300

    feat: search for images in the overview page

--- apps/frontend/src/components/pages/Overview.tsx
+++ apps/frontend/src/components/pages/Overview.tsx
@@ -1,3 +1,23 @@
+import { auth } from '@/auth/client';
+import { api } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+import type { Memory } from '@/types/routes';
+
 export default function Overview() {
+  const { data: session } = auth.useSession();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['random-memory', session?.user.image],
+    queryFn: async () => {
+      const response = await api.get<Memory[]>('/memory/get-random-memories', {
+        auth: true,
+      });
+
+      if (response.ok && response.data && response.data.length) {
+        return response.data[0].url;
+      }
+    },
+  });
+
   return <div>Overview</div>;
 }
