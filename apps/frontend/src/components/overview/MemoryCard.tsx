b1e5764c2115634dda0842dd0e62ac6472ef371e
Bruno Aseff <brunoaseff2@gmail.com>
2025-12-06 16:27:12 -0300

    style: improve memory card

--- apps/frontend/src/components/overview/MemoryCard.tsx
+++ apps/frontend/src/components/overview/MemoryCard.tsx
@@ -2,6 +2,7 @@ import { ImageNotFound01Icon } from '@hugeicons/core-free-icons';
 import { HugeiconsIcon } from '@hugeicons/react';
 import Link from 'next/link';
 import { ProgressiveBlur } from '@/components/ui/progressive-blur';
+import { Button } from '../ui/button';
 
 export default function MemoryCard({
   url,
@@ -17,33 +18,41 @@ export default function MemoryCard({
           className="object-cover rounded-lg"
         />
       ) : (
-        <div className="relative">
+        <div className="relative min-h-[400px] flex items-center justify-center">
           <img
             src="/no-memories-placeholder.jpg"
             alt=""
             role="presentation"
-            className="object-cover rounded-lg"
+            className="absolute inset-0 object-cover rounded-lg w-full h-full"
           />
           <ProgressiveBlur
             direction="bottom"
-            className="pointer-events-none absolute bottom-0 left-0 h-[70%] w-full rounded-lg"
-            blurIntensity={1}
+            className="pointer-events-none absolute inset-0 rounded-lg"
+            blurIntensity={2}
           />
-          <div className="absolute bottom-6 right-6 flex flex-col items-end text-right">
-            <HugeiconsIcon
-              icon={ImageNotFound01Icon}
-              className="text-background mb-2"
-              color="currentColor"
-              strokeWidth={2}
-              size={42}
-            />
-            <h1 className="text-background font-bold">You have no memories</h1>
-            <Link
-              className="text-base w-fit text-link hover:underline transition-all duration-300 font-medium"
-              href="/memories"
-            >
-              Click here to upload your first!
-            </Link>
+          <div className="absolute inset-0 bg-foreground/40 rounded-lg" />
+
+          <div className="relative z-10 flex flex-col items-center text-center px-6">
+            <div className="flex items-center justify-center gap-2 mb-3">
+              <HugeiconsIcon
+                icon={ImageNotFound01Icon}
+                className="text-background/80"
+                color="currentColor"
+                strokeWidth={2}
+                size={24}
+              />
+              <h1 className="text-background font-semibold text-2xl">
+                You have no memories
+              </h1>
+            </div>
+            <Button asChild size="sm" variant="outline">
+              <Link
+                className="px-4 py-2 rounded-md font-medium"
+                href="/memories"
+              >
+                Click here to upload your first!
+              </Link>
+            </Button>
           </div>
         </div>
       )}
